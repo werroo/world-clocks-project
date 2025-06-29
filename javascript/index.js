cf1cbeac42d45b4269d0b286e46f15af43d8eb7a
Weronika <weronika_dabrowska@yahoo.co.uk>
2025-06-29 21:53:36 +0100

    Added a link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -53,7 +53,8 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/">All Cities</a>`;
 }
 
 updateTime();
